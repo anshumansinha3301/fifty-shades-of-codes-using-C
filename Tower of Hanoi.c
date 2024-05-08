9183655572e60922ffbc78fdd44ae9b844a103f2
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-05-08 22:30:02 +0530

    Create Tower of Hanoi.c

---
+++ Tower of Hanoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
+    if (n == 1) {
+        printf("Move disk 1 from rod %c to rod %c\n", from_rod, to_rod);
+        return;
+    }
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    printf("Move disk %d from rod %c to rod %c\n", n, from_rod, to_rod);
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of disks: ");
+    scanf("%d", &n);
+
+    towerOfHanoi(n, 'A', 'C', 'B');
+
+    return 0;
+}
